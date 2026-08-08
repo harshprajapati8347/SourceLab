32f178d55acca4c6cd7d3990e802d4643ffb616a
Harsh Prajapati <harshprajapati8347@gmail.com>
2026-08-08 13:03:34 +0530

    init: add initial project setup

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/health", (_req, res) => {
+  res.json({ status: "ok" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
